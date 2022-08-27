fdec0074a1fec7ddcd6467a34388ea1abb68c2c1
Tatwamasi Mishra <tatwamasi01@gmail.com>
2022-08-27 23:44:43 +0530

    dropdowns are done bro

--- src/main/java/com/example/unitconverter/TempCalculator.java
+++ src/main/java/com/example/unitconverter/TempCalculator.java
@@ -5,22 +5,35 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class TempCalculator extends AppCompatActivity{
 
     String[] units = {"Celsius","Fahrenheit","Kelvin"};
     Spinner to , from;
     EditText fval,tval;
-    int t_ind=1,f_ind=0;
+    String tv_from,tv_to;
+    TextView flabel,tlabel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tempcalculator_layout);
         to = findViewById(R.id.FromDropdown);
         from = findViewById(R.id.ToDropdown);
-        ArrayAdapter ad = new ArrayAdapter(this,android.R.layout.simple_spinner_item, units);
-        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter ad = new ArrayAdapter(this,android.R.layout.select_dialog_item, units);
+        ad.setDropDownViewResource(android.R.layout.select_dialog_item);
+        to.setPrompt("Select");
+        from.setPrompt("Select");
         to.setAdapter(ad);
         from.setAdapter(ad);
+
+        tv_from  = from.getSelectedItem().toString();
+        tv_to = to.getSelectedItem().toString();
+        flabel = findViewById(R.id.f_unit);
+        tlabel = findViewById(R.id.t_unit);
+        flabel.setText(tv_from);
+        tlabel.setText(tv_to);
+
+//        to.setOnItemSelectedListener();
     }
 }
